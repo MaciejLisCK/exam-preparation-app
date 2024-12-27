80bca83e8bb53c8f906629f582e2faacaac298ee
M̦͈͈̳̭͚͇̳̙̙͍̱͖̺͈ͥ̽̿̈́̇ͭ̾̍ͮ̒̓́ͯ͛͆ͅaciej Lis <accounts@maciejlis.com>
2024-12-27 23:28:57 +0000

    add showing correct answear on all required selected

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, input, InputSignal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Question } from '../question';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
   @Input() question!: Question;
+
+  selectedAnswears: any[] = [];
+
+  get hasSelectedAllAnswears() {
+    return this.selectedAnswears.filter(Boolean).length === this.question.correctAnsewarsCount;
+  } 
 }
