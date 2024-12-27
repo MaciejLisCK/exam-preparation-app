9a31019c1da532cf34ee023c8fba66dd46dbf23e
M̦͈͈̳̭͚͇̳̙̙͍̱͖̺͈ͥ̽̿̈́̇ͭ̾̍ͮ̒̓́ͯ͛͆ͅaciej Lis <accounts@maciejlis.com>
2024-12-27 00:26:52 +0000

    add navbar and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LearnComponent } from './learn/learn.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LearnComponent,
+    }
+];
