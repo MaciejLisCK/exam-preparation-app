1762e69a844eaa60997e479e0e44f05e5b9a9c41
M̦͈͈̳̭͚͇̳̙̙͍̱͖̺͈ͥ̽̿̈́̇ͭ̾̍ͮ̒̓́ͯ͛͆ͅaciej Lis <accounts@maciejlis.com>
2024-12-29 11:50:42 +0000

    added materials link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Store } from '@ngxs/store';
 import { LoadQuestionsAction } from './questions/questions.actions';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, NavbarComponent, ],
+  imports: [ RouterOutlet, NavbarComponent, PdfViewerModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { LearnComponent } from './learn/learn.component';
+import { MaterialsComponent } from './materials/materials.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LearnComponent,
+    },
+    {
+        path: 'learn',
+        component: LearnComponent,
+    },
+    {
+        path: 'materials',
+        component: MaterialsComponent,
     }
 ];
